e2836d572d99a75b90116ce6ac71cc1ec8121ef5
methy-dino <gutotlima@gmail.com>
2025-07-30 23:35:29 -0300

    interface de usuário implementada

--- popup_data.js
+++ popup_data.js
@@ -1,6 +1,8 @@
 /* arquivo reservado para carregar dados sobre datas salvas, usando chrome.storage.local.set*/
-document.getElementById("evento").addEventListener("click", pedir_evento);
-document.getElementById("timer").addEventListener("click", pedir_timer);
+const EVENTO_BTN = document.getElementById("evento");
+EVENTO_BTN.addEventListener("click", pedir_evento);
+const TIMER_BTN = document.getElementById("timer");
+TIMER_BTN.addEventListener("click", pedir_timer);
 function salvar(){
 	/* stub */
 }
@@ -13,9 +15,40 @@ function arch_test(){
 function enviar(){
 }
 function pedir_evento(){
-
-	console.log("TESTE EVENTO");
+	/* TODO: CONFERIR SE CONTAINER_EVENTO EXISTE */
+	const container_evento = document.createElement("div");
+	container_evento.id = "CONTAINER_EVENTO";
+	/* -------------------------------------------- */
+	const evento_dados = document.createElement("input");
+	evento_dados.type = "text";
+	evento_dados.id = "EVENTO_DADOS";
+	evento_dados.placeholder = "DD/MM/YYYY HH:MM";
+	container_evento.appendChild(evento_dados);
+	/* -------------------------------------------- */
+	const evento_confirmar = document.createElement("button");
+	evento_confirmar.textContent = "criar";
+	evento_confirmar.id = "EVENTO_CONFIRMAR"
+	/* -------------------------------------------- */
+	container_evento.appendChild(evento_confirmar);
+	EVENTO_BTN.after(container_evento);
+	console.log("INTERFACE DE EVENTO INICIALIZADA");
 }
 function pedir_timer(){
-	console.log("TESTE TIMER");
+	/* TODO: CONFERIR SE CONTAINER_TIMER EXISTE */
+	const container_timer = document.createElement("div");
+	container_timer.id = "CONTAINER_TIMER";
+	/* -------------------------------------------- */
+	const timer_dados = document.createElement("input");
+	timer_dados.type = "text";
+	timer_dados.id = "TIMER_DADOS";
+	timer_dados.placeholder = "HH:MM:SS";
+	container_timer.appendChild(timer_dados);
+	/* -------------------------------------------- */
+	const timer_confirmar = document.createElement("button");
+	timer_confirmar.textContent = "criar";
+	timer_confirmar.id = "TIMER_CONFIRMAR"
+	/* -------------------------------------------- */
+	container_timer.appendChild(timer_confirmar);
+	TIMER_BTN.after(container_timer);
+	console.log("INTERFACE DE TIMER INICIALIZADA");
 }
